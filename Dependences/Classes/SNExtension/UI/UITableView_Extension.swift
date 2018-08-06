21b3a1e02ac36ce5ffcf6f1791814b14e309b966
spectatorNan <spectatornan@gmail.com>
2018-08-06 11:01:21 +0800

    archive bug fix

--- Dependences/Classes/SNExtension/UI/UITableView_Extension.swift
+++ Dependences/Classes/SNExtension/UI/UITableView_Extension.swift
@@ -11,7 +11,7 @@ import class UIKit.UITableView
 import class UIKit.UIViewController
 import struct Foundation.IndexPath
 
-protocol ReusableView: class {
+public protocol ReusableView {
     static var reuseIdentifier: String {get}
 }
 
