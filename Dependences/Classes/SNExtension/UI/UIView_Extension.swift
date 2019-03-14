4876b8a1ede46ca1a66dcedafb0ba891447933e7
spec <spectatornan@gmail.com>
2019-03-14 15:33:26 +0800

    add dashline
    layer 画虚线

--- Dependences/Classes/SNExtension/UI/UIView_Extension.swift
+++ Dependences/Classes/SNExtension/UI/UIView_Extension.swift
@@ -42,3 +42,48 @@ extension UIView {
     
     
 }
+
+
+extension UIView {
+
+    /// 画虚线，调用之前请确保能获取到frame
+    /// 如果有多个calayer在当前空间 会使其他layer失效
+    /// - Parameters:
+    ///   - lineLength: 每一节线长
+    ///   - lineSpacing: 间距
+    ///   - lineColor: 颜色
+    ///   - isHorizontal: 是否水平
+    public func drawDashLine(lineLength: Int, lineSpacing: Int, lineColor: UIColor, isHorizontal: Bool = true) {
+
+        if let sublayers = self.layer.sublayers {
+            for sublayer in sublayers {
+                sublayer.removeFromSuperlayer()
+            }
+
+        }
+        //        self.layer.sublayers = nil
+
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.bounds = self.bounds
+        //        只要是CALayer这种类型,他的anchorPoint默认都是(0.5,0.5)
+        shapeLayer.anchorPoint = CGPoint(x: 0, y: 0)
+        //        shapeLayer.fillColor = UIColor.blue.cgColor
+        shapeLayer.strokeColor = lineColor.cgColor
+
+        //        shapeLayer.lineWidth = self.frame.size.height
+        //        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+
+        shapeLayer.lineDashPattern = [NSNumber(value: lineLength), NSNumber(value: lineSpacing)]
+
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        if isHorizontal {
+            path.addLine(to: CGPoint(x: self.frame.size.width, y: 0))
+        } else {
+            path.addLine(to: CGPoint(x: 0, y: self.frame.size.height))
+        }
+        shapeLayer.path = path
+        self.layer.addSublayer(shapeLayer)
+    }
+
+}
