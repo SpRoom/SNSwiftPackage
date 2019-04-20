4bb794af5e2fb11af936f6381c6138aeac52e3ea
spec <spectatornan@gmail.com>
2019-04-20 15:30:54 +0800

    Api Parse Support String

---
+++ Dependences/Classes/ApiManager/StringApiModel.swift
@@ -0,0 +1,18 @@
+//
+//  StringApiModel.swift
+//  zshSeedOne
+//
+//  Created by ios-spec on 2019/4/20.
+//  Copyright © 2019 zsh. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import SNSwiftPackage
+
+extension String: SNSwiftyJSONAble {
+
+    public init?(jsonData: JSON) {
+        self = jsonData.stringValue
+    }
+}
