163524bc9c081af5b6913403a31b7257e9c03584
spectatorMrZ <zhuchunan001@sina.com>
2021-03-24 18:35:24 +0800

    add color ext

--- Dependences/Classes/SNExtension/UI/Color_Extension.swift
+++ Dependences/Classes/SNExtension/UI/Color_Extension.swift
@@ -22,6 +22,41 @@ extension UIColor {
         self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff, alpha: alpha)
     }
     
+    public convenience init(hex: String) {
+        
+            let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+            var int: UInt64 = 0
+            Scanner(string: hex).scanHexInt64(&int)
+            let a, r, g, b: UInt64
+            switch hex.count {
+            case 3: // RGB (12-bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            default:
+                (a, r, g, b) = (1, 1, 1, 0)
+            }
+
+            self.init(
+                red: CGFloat(r) / 255,
+                green: CGFloat(g) / 255,
+                blue:  CGFloat(b) / 255,
+                alpha: CGFloat(a) / 255
+            )
+        }
+        
+    public convenience init(hex: UInt, alpha: CGFloat = 1) {
+                self.init(
+                    red: CGFloat((hex >> 16) & 0xff) / 255,
+                    green: CGFloat((hex >> 08) & 0xff) / 255,
+                    blue: CGFloat((hex >> 00) & 0xff) / 255,
+                    alpha: alpha
+                )
+            
+    }
+    
     /*
      convenience init(hexadecimalStr hex: String) {
      var cString = hex.trimmingCharacters(in:CharacterSet.whitespacesAndNewlines).uppercased()
