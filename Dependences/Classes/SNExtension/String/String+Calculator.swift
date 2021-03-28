caef01fe02cc5faa292d93f7d3173114ae554325
SpectatorNan <zhuchunan001@sina.com>
2021-03-28 23:45:23 +0800

    add calculator string size

---
+++ Dependences/Classes/SNExtension/String/String+Calculator.swift
@@ -0,0 +1,39 @@
+//
+//  String+Calculator.swift
+//  SNSwiftPackage
+//
+//  Created by Spec on 2021/3/28.
+//
+
+import Foundation
+
+extension String {
+    public func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+    
+        return ceil(boundingBox.height)
+    }
+
+    public func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
+extension NSAttributedString {
+    public func height(withConstrainedWidth width: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, context: nil)
+    
+        return ceil(boundingBox.height)
+    }
+
+    public func width(withConstrainedHeight height: CGFloat) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, context: nil)
+    
+        return ceil(boundingBox.width)
+    }
+}
